db1626c28b1dfb5335fa26389303aaf8f1209e6a
Miguel Amores <migueamores@gmail.com>
2023-10-17 09:53:59 -0500

    optimize params read file

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 import { thumbnails, base_house } from "./thumbnails.js";
 
-function showCamera() {
-  navigator.mediaDevices
-    .getUserMedia({
-      video: {
-        aspectRatio: { exact: 16 / 9 },
-        facingMode: { exact: "environment" },
-      },
-    })
-    .then((stream) => {
-      const cameraEl = document.getElementById("cameraEl");
-      cameraEl.srcObject = stream;
-      cameraEl.play();
-      cameraEl.muted = true;
-      cameraEl.volume = 0;
-    })
-    .catch((err) => {
-      console.log({ err });
-    });
-}
-
 function AddModelsFromUrl() {
   const querystring = window.location.search;
   const params = new URLSearchParams(querystring);
@@ -32,7 +12,6 @@ function AddModelsFromUrl() {
   houseId == 1
     ? el.setAttribute("position", "0 1.33 -0.3")
     : el.setAttribute("position", "0 1.5 -0.3");
-  // el.setAttribute("position", "0 1.3 -0.3");
   entityContainer.appendChild(el);
 
   if (houseId == 1) {
@@ -50,7 +29,6 @@ function AddModelsFromUrl() {
   }
 
   params.forEach((value, key) => {
-    console.log(key, value);
     if (key !== "step" && key !== "houseId") {
       const isValueInteger = Number.isInteger(Number(value));
       if (isValueInteger) {
@@ -60,7 +38,6 @@ function AddModelsFromUrl() {
         if (optionSelected.position) {
           const { x, y, z } = optionSelected.position[houseId];
           el.setAttribute("position", { x, y, z });
-          // el.object3D.rotation.y = THREE.MathUtils.degToRad(45);
         } else {
           el.setAttribute("position", "0 1.5 -0.3");
         }
@@ -68,7 +45,6 @@ function AddModelsFromUrl() {
         entityContainer.appendChild(el);
       } else {
         const parsedValues = JSON.parse(decodeURIComponent(value));
-        console.log(parsedValues);
         parsedValues.forEach((value) => {
           const el = document.createElement("a-entity");
           const optionSelected = thumbnails[key].options[value];
@@ -78,56 +54,20 @@ function AddModelsFromUrl() {
           if (optionSelected.position) {
             const { x, y, z } = optionSelected.position[houseId];
             el.setAttribute("position", { x, y, z });
-            // el.object3D.rotation.y = THREE.MathUtils.degToRad(45);
           } else {
             el.setAttribute("position", "0 1.5 -0.3");
           }
 
           if (optionSelected.rotation) {
-            const { x = 0, y = 0, z = 0 } = optionSelected.rotation;
-            // el.setAttribute("rotation", { x, y, z });
+            const { y = 0 } = optionSelected.rotation;
             el.object3D.rotation.y = THREE.MathUtils.degToRad(y);
           }
 
           entityContainer.appendChild(el);
         });
       }
-
-      //   if (isValueInteger) {
-      //     setThumbnails(Number(value), thumbnailKeyName[key]);
-      //   } else {
-      //     const parsedValues = JSON.parse(decodeURIComponent(value));
-      //     parsedValues?.forEach((value: number) => setThumbnails(value, thumbnailKeyName[key]));
-      //   }
-      // }
     }
   });
-
-  // if (searchParams.has('houseId')) {
-  //   setHouseId(Number(searchParams.get('houseId')));
-  // }
-  // if (searchParams.has('step')) {
-  //   setStep(Number(searchParams.get('step')));
-  // }
-
-  //   const thumbnailKeyName = getThumbnailKeyName();
-  //   console.log(thumbnailKeyName);
-
-  // params.forEach((value, key) => {
-  //   if (key !== 'houseId' && key !== 'step') {
-  //     const isValueInteger = Number.isInteger(Number(value));
-  //     if (isValueInteger) {
-  //       setThumbnails(Number(value), thumbnailKeyName[key]);
-  //     } else {
-  //       const parsedValues = JSON.parse(decodeURIComponent(value));
-  //       parsedValues?.forEach((value: number) => setThumbnails(value, thumbnailKeyName[key]));
-  //     }
-  //   }
-  // });
-  //   params.forEach((value, key) => {
-  //     console.log(key, value);
-  //   });
 }
 
-// showCamera();
 AddModelsFromUrl();
